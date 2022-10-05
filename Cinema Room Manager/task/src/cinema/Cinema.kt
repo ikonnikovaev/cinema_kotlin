e71621bb74831e6794c37840322e117309a0466e
elena <ikonnikovaev@gmail.com>
2022-10-05 13:04:56 +0300

    Stage 3: ticket price

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,16 +1,45 @@
 package cinema
 
+const val EXPENSIVE = 10
+const val CHEAP = 8
+const val SIZE_THRESHOLD = 60
+
 fun profit(nRows: Int, seatsPerRow: Int): Int {
-    val expensive = 10
-    val cheap = 8
-    val threshold = 60
     val totalSeats = nRows * seatsPerRow
-    if (totalSeats <= threshold) {
-        return totalSeats * expensive
+    if (totalSeats <= SIZE_THRESHOLD) {
+        return totalSeats * EXPENSIVE
     }
     val frontHalfSeats = nRows / 2 * seatsPerRow
     val backHalfSeats = (nRows - nRows / 2) * seatsPerRow
-    return  frontHalfSeats * expensive + backHalfSeats * cheap
+    return  frontHalfSeats * EXPENSIVE + backHalfSeats * CHEAP
+}
+
+fun get_price(nRows: Int, seatsPerRow: Int, row: Int, seat: Int): Int {
+    val totalSeats = nRows * seatsPerRow
+    if (totalSeats <= SIZE_THRESHOLD || (row <= nRows / 2) ) {
+        return EXPENSIVE
+    }
+    return CHEAP
+}
+
+fun print_room(nRows: Int, seatsPerRow: Int, row: Int, seat: Int):Unit {
+    println("Cinema:")
+    print(" ")
+    for (s in 1..seatsPerRow) {
+        print(" $s")
+    }
+    println()
+    for (r in 1..nRows) {
+        print("$r")
+        for (s in 1..seatsPerRow) {
+            if (r == row && s == seat) {
+                print(" B")
+            } else {
+                print(" S")
+            }
+        }
+        println()
+    }
 }
 
 
@@ -19,19 +48,11 @@ fun main() {
     val nRows = readln().toInt()
     println("Enter the number of seats in each row:")
     val seatsPerRow = readln().toInt()
-    val result = profit(nRows, seatsPerRow)
-    println("Total income:")
-    println("\$$result")
-
-
-    /*
-    println("Cinema:")
-    println("  1 2 3 4 5 6 7 8")
-    println("1 S S S S S S S S")
-    println("2 S S S S S S S S")
-    println("3 S S S S S S S S")
-    println("4 S S S S S S S S")
-    println("5 S S S S S S S S")
-    println("6 S S S S S S S S")
-    println("7 S S S S S S S S") */
+    print_room(nRows, seatsPerRow, -1, -1)
+    println("Enter a row number:")
+    val row = readln().toInt()
+    println("Enter a seat number in that row:")
+    val seat = readln().toInt()
+    println("Ticket price: \$${get_price(nRows, seatsPerRow, row, seat)}")
+    print_room(nRows, seatsPerRow, row, seat)
 }
